6bf9f9f3a6edf91fcd6d5d0d2e364233167e0d56
spiritman90 <phemmyadey@gmail.com>
2023-06-08 12:26:09 +0100

    update item and reset state

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ function onAddItemSubmit(e) {
     return;
   }
 
+  // Check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector(".edit-mode");
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove("edit-mode");
+    itemToEdit.remove();
+    isEditMode = false;
+  }
+
   addItemToDOM(newItem);
   addItemToStorage(newItem);
 
@@ -149,6 +159,7 @@ function filterItems(e) {
 }
 
 function checkUI() {
+  itemInput.value = "";
   const items = itemList.querySelectorAll("li");
 
   if (items.length === 0) {
@@ -158,6 +169,11 @@ function checkUI() {
     clearBtn.style.display = "block";
     itemFilter.style.display = "block";
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = "#333";
+
+  isEditMode = false;
 }
 
 function init() {
